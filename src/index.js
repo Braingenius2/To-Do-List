a1058c3009412b657ef15860163134cd40b570be
Braingenius2 <braingenius2@gmail.com>
2023-05-11 23:01:31 +0300

    Add structure to list

--- src/index.js
+++ src/index.js
@@ -1,14 +1,41 @@
 import _ from 'lodash';
 import './style.css';
 
-function component() {
-  const element = document.createElement('div');
+const tasks = [
+  {
+    description: 'Go to market',
+    completed: true,
+    index: 1,
+  },
+  {
+    description: 'Print articles',
+    completed: false,
+    index:2,
+  },
+  {
+    description: 'Build castle',
+    completed: true,
+    index: 3,
+  }
+]
 
-  // Lodash, now imported by this script
-  element.innerHTML = _.join(['Hello', 'webpack'], ' ');
-  element.classList.add('hello');
-
-  return element;
+function renderTasks() {
+  const tasksContainer = document.querySelector('.tasks');
+  tasks.forEach(function(task) {
+    const li = document.createElement('li');
+    li.classList.add('task');
+    const checkboxElement = document.createElement('input');
+    checkboxElement.index = task.index;
+    checkboxElement.checked = task.completed;
+    tasksContainer.appendChild(checkboxElement);
+    const descriptionElement = document.createElement('p');
+    descriptionElement.textContent = task.description;
+    descriptionElement.classList.add('task-description');
+    tasksContainer.innerHTML += `<span class="material-icons-outlined">more_vert</span>`;
+    if (task.completed) {
+      descriptionElement.classList.add('completed');
+    }
+  });
 }
 
-document.body.appendChild(component());
+renderTasks();
