8e7e0aeed3d719498501a4a741e301df1a182eb7
Braingenius2 <braingenius2@gmail.com>
2023-05-12 21:20:40 +0300

    Implement addTask method for Tasks class

--- src/tasks.js
+++ src/tasks.js
@@ -1,28 +1,15 @@
-import './style.css';
-import Tasks from './tasks.js';
+export default class Tasks {
+  constructor() {
+    this.tasks = JSON.parse(localStorage.getItem('tasks') || '[]');
+  }
 
-const tasks = [
-  // {
-  //   description: 'Go to market',
-  //   completed: true,
-  //   index: 1,
-  // },
-  // {
-  //   description: 'Print articles',
-  //   completed: false,
-  //   index: 2,
-  // },
-  // {
-  //   description: 'Build castle',
-  //   completed: true,
-  //   index: 3,
-  // },
-];
-
-function renderTasks() {
-  tasks.sort((a, b) => a.index - b.index);
-  const tasksContainer = document.querySelector('.tasks');
-  tasks.forEach((task) => {
+  addTask(description) {
+    const task = {
+      description: description,
+      completed: false,
+      index: this.tasks.length,
+    };
+    const tasksContainer = document.querySelector('.tasks');
     const li = document.createElement('li');
     li.classList.add('task');
     tasksContainer.appendChild(li);
@@ -36,10 +23,14 @@ function renderTasks() {
     descriptionElement.classList.add('task-description');
     li.appendChild(descriptionElement);
     li.innerHTML += '<i class="material-icons more-vert">more_vert</i>';
-    if (task.completed) {
-      descriptionElement.classList.add('completed');
-    }
-  });
-}
+  }
 
-renderTasks();
+  deleteTask(index) {
+
+  }
+
+  editTask(index) {
+
+  }
+
+}
