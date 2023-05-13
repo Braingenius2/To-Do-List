1fcaa37a98de82dbefa88ca455c4cd6bf2aafe81
Braingenius2 <braingenius2@gmail.com>
2023-05-13 14:43:57 +0300

    Fix eslint errors

--- src/index.js
+++ src/index.js
@@ -1,17 +1,11 @@
 import './style.css';
 import Tasks from './tasks.js';
 
-let tasks = [
-];
-
-// populate tasks array with data from local storage
-tasks = JSON.parse(localStorage.getItem('tasks') || '[]');
-
 // Create a new Tasks object instance
 const tasksObj = new Tasks();
 tasksObj.renderTasks();
 
-// Implement a click event listener both for enter key and click on the input element add to your list...
+// Implement a click event listener on newTaskElement
 const newTaskElement = document.querySelector('.new-task');
 newTaskElement.addEventListener('keydown', (event) => {
   if (event.key === 'Enter') {
@@ -28,7 +22,3 @@ newTaskElement.addEventListener('keydown', (event) => {
     tasksObj.renderTasks();
   }
 });
-
-// Implement a click event listener on the description element appropriately in tasks.js
-
-
--- src/tasks.js
+++ src/tasks.js
@@ -5,102 +5,102 @@ export default class Tasks {
 
   // Implement method to render or display tasks in the list container
   renderTasks() {
-  this.tasks.sort((a, b) => a.index - b.index);
-  localStorage.setItem('tasks', JSON.stringify(this.tasks));
-  const tasksContainer = document.querySelector('.tasks');
-  tasksContainer.innerHTML = '';
-  this.tasks.forEach((task) => {
-    const li = document.createElement('li');
-    li.classList.add('task');
-    tasksContainer.appendChild(li);
-    const checkboxElement = document.createElement('input');
-    checkboxElement.type = 'checkbox';
-    checkboxElement.dataset.index = task.index;
-    checkboxElement.checked = task.completed;
-    li.appendChild(checkboxElement);
-    const descriptionElement = document.createElement('p');
-    descriptionElement.textContent = task.description;
-    descriptionElement.classList.add('task-description');
-    li.appendChild(descriptionElement);
+    this.tasks.sort((a, b) => a.index - b.index);
+    localStorage.setItem('tasks', JSON.stringify(this.tasks));
+    const tasksContainer = document.querySelector('.tasks');
+    tasksContainer.innerHTML = '';
+    this.tasks.forEach((task) => {
+      const li = document.createElement('li');
+      li.classList.add('task');
+      tasksContainer.appendChild(li);
+      const checkboxElement = document.createElement('input');
+      checkboxElement.type = 'checkbox';
+      checkboxElement.dataset.index = task.index;
+      checkboxElement.checked = task.completed;
+      li.appendChild(checkboxElement);
+      const descriptionElement = document.createElement('p');
+      descriptionElement.textContent = task.description;
+      descriptionElement.classList.add('task-description');
+      li.appendChild(descriptionElement);
 
-    // Implement a click event listener on the description element
-    descriptionElement.addEventListener('click', () => {
+      // Implement a click event listener on the description element
+      descriptionElement.addEventListener('click', () => {
       // Set the contentEditable property to true and focus on the element
-      descriptionElement.contentEditable = true;
-      descriptionElement.focus();
+        descriptionElement.contentEditable = true;
+        descriptionElement.focus();
 
-      // Remove outline from the element
-      descriptionElement.style.outline = 'none';
+        // Remove outline from the element
+        descriptionElement.style.outline = 'none';
 
-      // Store original description in a variable, in case user cancels edit
-      const originalDescription = descriptionElement.textContent;
+        // Store original description in a variable, in case user cancels edit
+        const originalDescription = descriptionElement.textContent;
 
-      // Add  blur event listener to save the edited task description
-      descriptionElement.addEventListener('blur', () => {
+        // Add  blur event listener to save the edited task description
+        descriptionElement.addEventListener('blur', () => {
         // Get the new description from the element
-        const newDescription = descriptionElement.textContent;
+          const newDescription = descriptionElement.textContent;
 
-        // Set the contentEditable property back to false
-        descriptionElement.contentEditable = false;
+          // Set the contentEditable property back to false
+          descriptionElement.contentEditable = false;
 
-        // If new description isn't empty and different from the original
-        // Update task and re-render tasks
-        if (newDescription.trim() !== '' && newDescription !== originalDescription) {
-          this.editTask(newDescription, task.index);
-          this.renderTasks();
-        } else {
-          descriptionElement.textContent = originalDescription;
-          this.renderTasks();
-        }
+          // If new description isn't empty and different from the original
+          // Update task and re-render tasks
+          if (newDescription.trim() !== '' && newDescription !== originalDescription) {
+            this.editTask(newDescription, task.index);
+            this.renderTasks();
+          } else {
+            descriptionElement.textContent = originalDescription;
+            this.renderTasks();
+          }
+        });
+
+        // Add event listener to save the edited task description
+        descriptionElement.addEventListener('keydown', (event) => {
+          if (event.key === 'Enter') {
+            event.preventDefault();
+
+            // Trigger the blur event to save the edited task description
+            descriptionElement.blur();
+          }
+        });
+
+        // Also replace 3 vertical dots icon in the li container with a delete icon
+        const rightElement = li.querySelector('.delete-container');
+        const deleteIcon = rightElement.querySelector('.delete');
+        deleteIcon.style.display = 'block';
+        const moreVert = rightElement.querySelector('.more-vert');
+        moreVert.style.display = 'none';
+
+        // Also toggle yellow background
+        li.classList.add('clicked-task');
       });
+      const rightElement = document.createElement('div');
+      rightElement.classList.add('delete-container');
+      rightElement.dataset.index = task.index;
+      li.appendChild(rightElement);
+      rightElement.innerHTML = '<i class="material-icons more-vert">more_vert</i><i class="material-icons delete">delete</i>';
 
-      // Add keydown event listener if enter key is used by user to save the edited task description
-      descriptionElement.addEventListener('keydown', (event) => {
-        if (event.key === 'Enter') {
-          event.preventDefault();
-          
-          // Trigger the blur event to save the edited task description
-          descriptionElement.blur();
-        }
+      // Add click event listener to deleteIcon
+      // first deactivate click on rightElement
+      rightElement.addEventListener('click', (event) => {
+        event.preventDefault();
       });
-
-      // Also replace 3 vertical dots icon in the li container with a delete icon
-      const rightElement = li.querySelector('.delete-container');
       const deleteIcon = rightElement.querySelector('.delete');
-      deleteIcon.style.display = 'block';
-      const moreVert = rightElement.querySelector('.more-vert');
-      moreVert.style.display = 'none';
+      deleteIcon.addEventListener('click', (event) => {
+        event.preventDefault();
+        this.deleteTask(task.index);
+        this.renderTasks();
+      });
 
-      // Also toggle yellow background
-      li.classList.add('clicked-task');
+      if (task.completed) {
+        descriptionElement.classList.add('completed');
+      }
     });
-    const rightElement = document.createElement('div');
-    rightElement.classList.add('delete-container');
-    rightElement.dataset.index = task.index;
-    li.appendChild(rightElement);
-    rightElement.innerHTML = `<i class="material-icons more-vert">more_vert</i><i class="material-icons delete">delete</i>`;
-    
-    // Add click event listener to deleteIcon
-    //first deactivate click on rightElement
-    rightElement.addEventListener('click', (event) => {
-      event.preventDefault();
-    });
-    const deleteIcon = rightElement.querySelector('.delete');
-    deleteIcon.addEventListener('click', (event) => {
-      event.preventDefault();
-      this.deleteTask(task.index);
-      this.renderTasks();
-    });
-
-    if (task.completed) {
-      descriptionElement.classList.add('completed');
-    }
-  });
   }
 
   addTask(description) {
     const task = {
-      description: description,
+      description,
       completed: false,
       index: this.tasks.length + 1,
     };
@@ -113,10 +113,10 @@ export default class Tasks {
     this.tasks = this.tasks.filter((task) => task.index !== index);
 
     // update index of remaining objects so they represent the current list order
-    for (let i = 0; i < this.tasks.length; i++) {
+    for (let i = 0; i < this.tasks.length; i += 1) {
       this.tasks[i].index = i + 1;
     }
-    localStorage.setItem('tasks', JSON.stringify(this.tasks))
+    localStorage.setItem('tasks', JSON.stringify(this.tasks));
   }
 
   // deleteTask(index) {
@@ -128,11 +128,10 @@ export default class Tasks {
   // }
 
   editTask(newDescription, index) {
-    const objectIndex = this.tasks.findIndex(task => task.index === index);
+    const objectIndex = this.tasks.findIndex((task) => task.index === index);
     if (objectIndex !== -1) {
       this.tasks[objectIndex].description = newDescription;
       localStorage.setItem('tasks', JSON.stringify(this.tasks));
     }
   }
-
 }
